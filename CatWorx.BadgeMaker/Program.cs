b909b88497116057efb1e223aec1b9643b52d340
Christian Castillo <chriscast94@outlook.com>
2022-04-30 18:27:07 -0700

    added lessons

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -15,58 +15,86 @@ using System.Collections.Generic;
 //Namespace similar to modules in Node.js
 //Like containers with members — everything within the curly braces are members within that container
 
-//Example Dictionary
-Dictionary<string, int> myScoreBoard = new Dictionary<string, int>();
+// //Example Dictionary
+// Dictionary<string, int> myScoreBoard = new Dictionary<string, int>();
 
-myScoreBoard.Add("firstInning", 10);
-myScoreBoard.Add("secondInning", 20);
-myScoreBoard.Add("thirdInning", 30);
-myScoreBoard.Add("fourthInning", 40);
-myScoreBoard.Add("fifthInning", 50);
+// myScoreBoard.Add("firstInning", 10);
+// myScoreBoard.Add("secondInning", 20);
+// myScoreBoard.Add("thirdInning", 30);
+// myScoreBoard.Add("fourthInning", 40);
+// myScoreBoard.Add("fifthInning", 50);
 
-Console.WriteLine("----------------");
-Console.WriteLine("  Scoreboard");
-Console.WriteLine("----------------");
-Console.WriteLine("Inning |  Score");
-Console.WriteLine("   1   |    {0}", myScoreBoard["firstInning"]);
-Console.WriteLine("   2   |    {0}", myScoreBoard["secondInning"]);
-Console.WriteLine("   3   |    {0}", myScoreBoard["thirdInning"]);
-Console.WriteLine("   4   |    {0}", myScoreBoard["fourthInning"]);
-Console.WriteLine("   5   |    {0}", myScoreBoard["fifthInning"]);
+// Console.WriteLine("----------------");
+// Console.WriteLine("  Scoreboard");
+// Console.WriteLine("----------------");
+// Console.WriteLine("Inning |  Score");
+// Console.WriteLine("   1   |    {0}", myScoreBoard["firstInning"]);
+// Console.WriteLine("   2   |    {0}", myScoreBoard["secondInning"]);
+// Console.WriteLine("   3   |    {0}", myScoreBoard["thirdInning"]);
+// Console.WriteLine("   4   |    {0}", myScoreBoard["fourthInning"]);
+// Console.WriteLine("   5   |    {0}", myScoreBoard["fifthInning"]);
 
 
 
-// namespace CatWorx.BadgeMaker
-// {
-//     class Program
-//     {
-//         static void Main(string[] args)
-//         {
-//             // Integers
-//             // Console.WriteLine(2 * 3);         // Basic Arithmetic: +, -, /, * 6
-//             // Console.WriteLine(10 % 3);        // Modulus Op => remainder of 10/3 1
-//             // Console.WriteLine(1 + 2 * 3);     // order of operations 7
-//             // Console.WriteLine(10 / 3.0);      // int's and doubles 3.3
-//             // Console.WriteLine(10 / 3);        // int's  3
-//             // Console.WriteLine("12" + "3");    // What happens here? strings concat
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //-----------------------------------------------------------------------------------------//
 
-//             // int num = 10;
-//             // num += 100; // adds 100 to int 10
-//             // Console.WriteLine(num);
-//             // num++; //adds 1 to int num
-//             // Console.WriteLine(num);
+            // Integers
+            // Console.WriteLine(2 * 3);         // Basic Arithmetic: +, -, /, * 6
+            // Console.WriteLine(10 % 3);        // Modulus Op => remainder of 10/3 1
+            // Console.WriteLine(1 + 2 * 3);     // order of operations 7
+            // Console.WriteLine(10 / 3.0);      // int's and doubles 3.3
+            // Console.WriteLine(10 / 3);        // int's  3
+            // Console.WriteLine("12" + "3");    // What happens here? strings concat
 
-//             //Boolean
-//             // bool isCold = true;
-//             // Console.WriteLine(isCold ? "drink" : "add ice");  // output: drink
-//             // Console.WriteLine(!isCold ? "drink" : "add ice");  // output: add ice
+            // int num = 10;
+            // num += 100; // adds 100 to int 10
+            // Console.WriteLine(num);
+            // num++; //adds 1 to int num
+            // Console.WriteLine(num);
 
-//             //Conversion
-//             string stringNum = "2";
-//             int intNum = Convert.ToInt16(stringNum); //Convert begins conversion, .ToInt16 converts to specific type
-//             Console.WriteLine(intNum);
-//             Console.WriteLine(intNum.GetType());
+            //Boolean
+            // bool isCold = true;
+            // Console.WriteLine(isCold ? "drink" : "add ice");  // output: drink
+            // Console.WriteLine(!isCold ? "drink" : "add ice");  // output: add ice
 
-//         }
-//     }
-// }
+            //Conversion
+            // string stringNum = "2";
+            // int intNum = Convert.ToInt16(stringNum); //Convert begins conversion, .ToInt16 converts to specific type
+            // Console.WriteLine(intNum);
+            // Console.WriteLine(intNum.GetType());
+
+            //---------------------------------------------------------------//
+
+            //Declaring an Array Example
+            // string[] favFoods = new string[3] { "pizza", "doughnuts", "icecream" };
+            // string firstFood = favFoods[0];
+            // string secondFood = favFoods[1];
+            // string thirdFood = favFoods[2];
+            // Console.WriteLine("I like {0}, {1}, and {2}", firstFood, secondFood, thirdFood);
+
+            //---------------------------------------------------------------//
+
+            //Declaring a List Example
+
+            List<string> employees = new List<string>() { "adam", "amy" };
+
+            employees.Add("barbara");
+            employees.Add("billy");
+            // Console.WriteLine("My employees include {0}, {1}, {2}, and {3}!", employees[0], employees[1], employees[2], employees[3]);
+
+            //Loops example
+            for (int i = 0; i < employees.Count; i++)
+            {
+                Console.WriteLine(employees[i]);
+            }
+
+
+        }
+    }
+}
